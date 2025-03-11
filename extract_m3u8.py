754f5c558e1f48b85fa1962faf307d55f7dec946
MEGA-GROUP <uzagdasan@mail.ru>
2025-03-12 01:08:16 +0400

    Update extract_m3u8.py

--- extract_m3u8.py
+++ extract_m3u8.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # Hedef site URL'si
-url = "https://raw.githubusercontent.com/ipstreet312/freeiptv/refs/heads/master/all.m3u"
+url = "http://65.21.85.175:8000/playlist.m3u8?token=livetv"
 
 # Klasör ve dosya adı
 output_folder = "yayin_linkleri"
