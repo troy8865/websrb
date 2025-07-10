f09cfe712b388e01aaa0890c505918260a2a0778
MEGA-GROUP <uzagdasan@mail.ru>
2025-07-11 02:40:37 +0400

    Create showtv.php

---
+++ showtv.php
@@ -0,0 +1,27 @@
+<?php
+$kanal = $_GET['kanal'] ?? 'kinomiks';
+
+function getM3U8Link($kanal) {
+    // Məsələn, bu URL-də canlı tokenli m3u8 var
+    $source_url = "http://site.com/player.php?id=" . urlencode($kanal);
+
+    // Qaynaq HTML-i al
+    $html = file_get_contents($source_url);
+
+    // M3U8 linki çıxar (regex ilə)
+    if (preg_match('/(https?:\/\/[^\s\'"]+\.m3u8[^\'"\s]*)/', $html, $matches)) {
+        return $matches[1]; // Tapılan link
+    }
+
+    return false;
+}
+
+$link = getM3U8Link($kanal);
+
+if ($link) {
+    // Əgər link tapıldısa yönləndir
+    header("Location: $link");
+    exit;
+} else {
+    echo "Link tapılmadı";
+}
