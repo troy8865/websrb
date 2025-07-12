19fa952b8b655f0a79e7b7ed23bb0efd79254e4d
MEGA-GROUP <uzagdasan@mail.ru>
2025-07-12 15:49:00 +0400

    Update extract_m3u8.py

--- extract_m3u8.py
+++ extract_m3u8.py
@@ -10,7 +10,7 @@ source_urls = {
     "kanal7": "https://www.kanal7.com/canli-izle",
     "showturk": "https://www.showturk.com.tr/canli-yayin/showturk",
     "marneulitv": "http://158.101.222.193:88/georgia_play.php?id=marneulitv",
-    "ciftcitv": "https://izle.ciftcitv.com/canli/",
+    "Sahar": "https://sepehrtv.ir/live/sahar1",
     "beyaztv": "https://beyaztv.com.tr/canli-yayin",
     # Digər kanallar əlavə edə bilərsən
 }
